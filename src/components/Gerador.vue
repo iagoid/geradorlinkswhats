0a3ff0557c4a12d8fa9ebc2b7fbab7f6ab436e61
iagoid <iagoid01@gmail.com>
2020-07-25 12:55:52 -0300

    first commit

---
+++ src/components/Gerador.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-12 py-5">
+    <div class="form-group">
+      <label for="">NÚMERO</label>
+    <input type="text" v-model="numero" class="form-control" name="" id="">
+    </div>
+    <div class="form-group">
+      <label for="">MENSAGEM</label>
+    <input type="text" v-model="mensagem" class="form-control" name="" id="">
+    </div>
+    <button @click="GerarLink" class="btn btn-primary btn-lg btn-block">GERAR LINK</button>
+  {{url}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gerador",
+  data: function() {
+    return {
+      numero: "",
+      mensagem: "",
+      url: "",
+    };
+  },
+  methods: {
+    GerarLink: function() {
+      // Substitui tudo o que nã é numero por ''
+      let n = this.numero.replace(/[^0-9]/g, '');
+      // Limpa a mensagem
+      let m = encodeURI(this.mensagem);
+      // Link do whatsapp
+      this.url = `http://wa.me/send?phone=${n}&text=${m}`;
+      // Chama a página about
+      this.$router.push({
+        name: "Resultado",
+        params: {
+          link: this.url,
+      },
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/Resultado.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">SEU LINK FOI GERADO</label>
+          <textarea
+            class="form-control"
+            id="url"
+            rows="3"
+            v-model="$route.params.link"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 d-flex justify-content-around">
+      <!-- data-clipboard indica o que será copiado -->
+      <button class="btn btn-success" id="copiar" data-clipboard-target="#url">COPIAR</button>
+      <a target="_blank" :href="$route.params.link"><button class="btn btn-warning">TESTAR</button></a>
+      <router-link to="/" class="btn btn-danger">VOLTAR</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClipboardJS from "clipboard";
+// Vai estar em todo botão com id  copiar
+new ClipboardJS("#copiar")
+export default {};
+</script>
